742e446b75e2a244150c4c4fb53ebdc068620078
Suzu Nohara <suzu.nkdca@gmail.com>
2023-06-16 22:13:35 -0600

    formularios, informacion y acciones de juego

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Controles_Isla, Isla, Produccion } from 'src/app/dto/Isla';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Acciones, Controles_Isla, Isla, Produccion } from 'src/app/dto/Isla';
+import { IslaComponent } from '../isla/isla.component';
+import { GameService } from 'src/app/service/game.service';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +11,18 @@ import { Controles_Isla, Isla, Produccion } from 'src/app/dto/Isla';
 export class HomeComponent implements OnInit{
 
   public isla: Isla;
-  public produccion: Produccion;
-  public controIsla: Controles_Isla;
+  public actions: Acciones;
+  @ViewChild(IslaComponent) isComp!: IslaComponent;
 
-  constructor() {
+  constructor(private game: GameService) {
     this.isla = new Isla();
-    this.produccion = new Produccion();
-    this.controIsla = new Controles_Isla();
+    this.actions = new Acciones();
+    this.isla.inversion = new Controles_Isla();
+    this.isla.inversion.produccion_interna = new Produccion();
     this.isla.poblacion = 1;
-    this.produccion.tecnologia = 10;
-    this.controIsla.infraestructura = 1;
-  }
+    this.isla.inversion.produccion_interna.tecnologia = 10;
+    this.isla.inversion.infraestructura = 1;
+  }// funcion de isla inicial (parametros iniciales de la isla)
 
   ngOnInit(): void {
     
@@ -36,6 +39,15 @@ export class HomeComponent implements OnInit{
     if (mobileMenu) {
       mobileMenu.style.width = "0%";
     }
+  }
+
+  public terminarTurno(){
+    // el usuario selecciona sus acciones del turno
+    // se llama la funcion de juego para que procese el turno
+    this.isla = this.game.turno(this.isla, this.actions);
+    // se obtiene la isla con informacion actualizada
+    // esta informacion se manda al componente de isla
+    this.isComp.updateIsla(this.isla);
   }
 }
 
--- src/app/components/isla/isla.component.ts
+++ src/app/components/isla/isla.component.ts
@@ -9,25 +9,22 @@ import { Produccion, Controles_Isla, Isla } from 'src/app/dto/Isla';
 export class IslaComponent implements OnInit {
 
   public isla: Isla;
-  public produccion: Produccion;
-  public controIsla: Controles_Isla;
 
   @Input() status: Isla;
-  @Input() statusP: Produccion;
-  @Input() statusCI: Controles_Isla;
 
   constructor() {
     this.isla = new Isla();
-    this.produccion = new Produccion();
-    this.controIsla = new Controles_Isla();
     this.status = new Isla();
-    this.statusP = new Produccion();
-    this.statusCI = new Controles_Isla();
   }
 
   ngOnInit() {
-    this.isla.poblacion = this.status.poblacion;
-    this.produccion.tecnologia = this.statusP.tecnologia;
-    this.controIsla.infraestructura = this.statusCI.infraestructura;
+    this.updateIsla(this.status);
+  }
+
+  public updateIsla(newIsla: Isla){
+    this.isla = newIsla;
+    // this.isla.poblacion = this.status.poblacion;
+    // this.isla.inversion.produccion_interna.tecnologia = this.status.inversion.produccion_interna.tecnologia;
+    // this.isla.inversion.infraestructura = this.status.inversion.infraestructura;
   }
 }
