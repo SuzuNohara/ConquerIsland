5ac7005cf6947f0792bc1577af623529bd48bde4
EnriqueTM04 <enriquetabo03@gmail.com>
2023-07-26 16:59:04 -0600

    Ordenar datos

--- src/app/service/backend.service.ts
+++ src/app/service/backend.service.ts
@@ -24,7 +24,30 @@ export class BackendService {
     });
   }
 
-  public getScore(){
-
+  public getScore(puntuaciones: Puntuacion[]): Promise<Puntuacion[]>{
+    return new Promise((resolve) => {
+      this.quickSort(puntuaciones);
+      resolve(puntuaciones);
+    });
   }
+  
+  private quickSort(arr: Puntuacion[]): Puntuacion[] {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].turnos < pivot.turnos) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    } 
+    return [...this.quickSort(left), pivot, ...this.quickSort(right)];
+  }
+
 }
